76ba0884b9d6910195d11f66d1dae6f7686bb924
Alexander Ulizko <alexander@ulizko.com>
2018-07-02 21:17:32 +0300

    Make bronze status magenta color

--- zsh/style.zsh
+++ zsh/style.zsh
@@ -5,7 +5,7 @@ export CLICOLOR=true
 RIPZ_TEXT=💡
 
 # https://github.com/reujab/bronze/
-BRONZE=(status:black:white dir:blue:black git:green:black cmdtime:yellow:black)
+BRONZE=(status:magenta:white dir:blue:black git:green:black cmdtime:yellow:black)
 export BRONZE_DIR_LENGTH=5
 export BRONZE_CMDTIME_THRESHOLD=1s
 export BRONZE_SHELL=zsh
