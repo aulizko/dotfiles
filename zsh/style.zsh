27b5af3668ede7d3202029f9e8064217a7afbd39
Alexander Ulizko <alexander@ulizko.com>
2022-05-30 13:50:44 +0300

    Move from silver prompt to starship

--- zsh/style.zsh
+++ zsh/style.zsh
@@ -4,10 +4,5 @@ export CLICOLOR=true
 # https://github.com/jedahan/ripz
 RIPZ_TEXT=💡
 
-# https://github.com/reujab/silver/
-SILVER=(status:red:white time:magenta:black dir:blue:black git:green:black cmdtime:yellow:black)
-export SILVER_DIR_LENGTH=5
-export SILVER_CMDTIME_THRESHOLD=15s
-export SILVER_SHELL=zsh
-
-eval "$(silver init)"
+# https://starship.rs/
+eval "$(starship init zsh)"
