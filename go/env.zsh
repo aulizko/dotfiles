08d6d8814c85ec29a316b254bd61f906cfde3fce
Alexander Ulizko <alexander@ulizko.com>
2022-05-30 15:10:54 +0300

    Some private settings for Golang and CGO.

--- go/env.zsh
+++ go/env.zsh
@@ -2,3 +2,8 @@
 
 export GOPATH=$PROJECTS/go
 export PATH="$GOPATH/bin:$PATH"
+
+# Allow cgo for local development
+export CGO_CXXFLAGS_ALLOW=".*"
+export CGO_LDFLAGS_ALLOW=".*"
+export CGO_CFLAGS_ALLOW=".*"
